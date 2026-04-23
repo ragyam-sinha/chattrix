a40e1979690e57fa8c84c4e4735b9ea18ed2fced
ragyam-sinha <ragyamsinha@gmail.com>
2026-04-23 20:52:48 +0530

    Fix Netlify build reliability

--- server/models/User.js
+++ server/models/User.js
@@ -16,9 +16,5 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.index({ chatrixId: 1 }, { unique: true });
-userSchema.index({ googleId: 1 }, { unique: true });
-userSchema.index({ email: 1 }, { unique: true });
-
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 export default User;
